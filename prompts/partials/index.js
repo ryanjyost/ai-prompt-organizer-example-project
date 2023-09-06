9f57118813378a877600f7eabc1d04150609ca74
ryanjyost <ryost217@gmail.com>
2023-09-05 21:43:19 -0600

    wip

--- prompts/partials/index.js
+++ prompts/partials/index.js
@@ -1,2 +1,3 @@
+// This will be merged with the partials file
 module.exports.partialInFolder =
   "This is a partial in a folder and will be merged with default export as well as a top-level partials.js file";
