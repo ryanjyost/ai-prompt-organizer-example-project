856af82e4f59229826516d36c64f8d982219ba1c
ryanjyost <ryost217@gmail.com>
2023-09-06 08:52:23 -0600

    wip

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const path = require("path");
-const PromptOrganizer = require("ai-prompt-organizer");
+import path from "path";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+import PromptOrganizer from "ai-prompt-organizer";
 
-const config = {
-  source: path.join(__dirname, "prompts"),
-  debug: true,
-};
-
-PromptOrganizer.init(config);
+// optional config and init, these are the defaults
+// const config = {
+//   source: path.join(__dirname, "prompts"),
+//   debug: true,
+// };
+// PromptOrganizer.init(config);
 
 async function runExample() {
   /* Get and prompt */
@@ -14,13 +16,10 @@ async function runExample() {
   // "Hello World!"
 
   /* Input variables */
-  PromptOrganizer.use(
-    `The inputs are {{inputs.one}} and {{inputs.two}}`,
-    {
-      one: "ONE VALUE",
-      two: "TWO VALUE",
-    }
-  );
+  PromptOrganizer.use(`The inputs are {{inputs.one}} and {{inputs.two}}`, {
+    one: "ONE VALUE",
+    two: "TWO VALUE",
+  });
   // "The inputs are ONE VALUE and TWO VALUE"
 
   PromptOrganizer.get("promptWithInputs", {
@@ -42,10 +41,17 @@ async function runExample() {
   /* Access any prompt inside of another prompt */
   PromptOrganizer.use(
     `{{partials.reusableGreeting}}! My name is Ryan. I am {{constants.age}} years old. {{Level1Prompts.Level2Prompts.level2Prompt}} The current time is {{inputs.currentTimestamp}}.
-`,
+  `,
     { currentTimestamp: Date.now() }
   );
   // "Hello Chat GPT! My name is Ryan. I am 30 years old. This is a level 2 prompt. The current time is *insert timestamp here*"".
+
+  const inputs = { question: "Why do zebras have stripes?", age: 10 };
+  PromptOrganizer.get("fullPrompt", inputs);
+  // "Hello World!
+  // This is a level 1 prompt.
+  // Hello Chat GPT.
+  // 30"
 }
 
 runExample();
---
+++ prompts/index.cjs
@@ -0,0 +1,48 @@
+const objectOfPrompts = {
+  important: "IMPORTANT!",
+  helloWorld: "Hello World!",
+  topLevelPrompt: "This is a top level prompt.",
+  Level1Prompts: {
+    level1Prompt: "This is a level 1 prompt.",
+    Level2Prompts: {
+      level2Prompt: "This is a level 2 prompt.",
+    },
+  },
+  greetings: {
+    hello: "Hi GPT!",
+    poseQuestion: "I have a question.",
+  },
+  respondInStyle: "Write your response in the style of",
+  writingStyles: {
+    mobster: "a mobster from the 1920s",
+    celebs: {
+      misterRogers: "Mr. Rogers",
+    },
+  },
+  ageAppropriate:
+    "{{important}} Make your response appropriate for someone who is {{inputs.age}} years old.",
+  fullPrompt: `
+    {{greetings.hello}} {{greetings.poseQuestion}}
+    {{inputs.question}}
+    {{respondInStyle}} {{writingStyles.celebs.misterRogers}}
+    {{ageAppropriate}}
+    {{inputs.age}}
+`,
+};
+
+const promptWithInputs = `
+  This prompt has two inputs: {{inputs.one}} and {{inputs.two}}
+`;
+
+const promptThatUsesOtherPrompts = `
+  {{helloWorld}}
+  {{Level1Prompts.level1Prompt}}
+  {{partials.reusableGreeting}}
+  {{constants.age}}
+`;
+
+module.exports = {
+  ...objectOfPrompts,
+  promptWithInputs,
+  promptThatUsesOtherPrompts,
+};
